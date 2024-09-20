023362fd2d6f962e7ad359efd219561402456508
alkautsar.efendi <NHSEC2018>
2024-09-20 17:32:39 +0700

    Design Layout MainActivity

--- app/src/main/java/project/alkautsar/simulasikredit/MainActivity.kt
+++ app/src/main/java/project/alkautsar/simulasikredit/MainActivity.kt
@@ -21,10 +21,5 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen().setKeepOnScreenCondition { splashViewModel.isLoading.value }
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
